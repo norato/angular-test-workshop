b780f9d351a541031f56e886d6654035928fad03
Felipe Norato <fnoratol@gmail.com>
2020-05-17 18:39:48 -0300

    Add more methods at delete

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { NgHttpLoaderModule } from 'ng-http-loader';
 
 import { MaterialModule } from '../material/material.module';
 import { SharedModule } from '../shared/shared.module';
@@ -9,6 +10,12 @@ import { UsersComponent } from './users.component';
 
 @NgModule({
   declarations: [UsersComponent, ListUsersComponent],
-  imports: [CommonModule, UsersRoutingModule, SharedModule, MaterialModule],
+  imports: [
+    CommonModule,
+    UsersRoutingModule,
+    SharedModule,
+    MaterialModule,
+    NgHttpLoaderModule.forRoot(),
+  ],
 })
 export class UsersModule {}
