35484b9be46afdf255d812a8816a9b53150307a6
Felipe Norato <fnoratol@gmail.com>
2020-05-17 19:43:09 -0300

    Edit user

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,22 +1,31 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgHttpLoaderModule } from 'ng-http-loader';
 
 import { MaterialModule } from '../material/material.module';
 import { SharedModule } from '../shared/shared.module';
+import { UsersComponent } from './index/users.component';
 import { ListUsersComponent } from './list-users/list-users.component';
+import { UserEditComponent } from './user-edit/user-edit.component';
 import { UserViewComponent } from './user-view/user-view.component';
 import { UsersRoutingModule } from './users-routing.module';
-import { UsersComponent } from './users.component';
 
 @NgModule({
-  declarations: [UsersComponent, ListUsersComponent, UserViewComponent],
+  declarations: [
+    UsersComponent,
+    ListUsersComponent,
+    UserViewComponent,
+    UserEditComponent,
+  ],
   imports: [
     CommonModule,
     UsersRoutingModule,
     SharedModule,
     MaterialModule,
     NgHttpLoaderModule.forRoot(),
+    FormsModule,
+    ReactiveFormsModule,
   ],
   entryComponents: [UserViewComponent],
 })
