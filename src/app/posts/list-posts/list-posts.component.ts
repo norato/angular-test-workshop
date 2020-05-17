9cebbd3731da5a05dda1416e775a201f2f5f58e6
Felipe Norato <fnoratol@gmail.com>
2020-05-17 17:50:31 -0300

    Connect confirmation dialog

--- src/app/posts/list-posts/list-posts.component.ts
+++ src/app/posts/list-posts/list-posts.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { filter, switchMap, tap } from 'rxjs/operators';
 
 import { PostService } from '../post.service';
+import { ConfirmationDialogComponent } from './../../shared/components/confirmation-dialog/confirmation-dialog.component';
 
 @Component({
   selector: 'app-list-posts',
@@ -11,12 +14,44 @@ export class ListPostsComponent implements OnInit {
   dataSource$ = this.postService.entities$;
   columns = ['id', 'title'];
 
-  constructor(private readonly postService: PostService) {}
+  constructor(
+    private readonly postService: PostService,
+    public dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
     this.postService.getPosts();
   }
+
   tableActions(action) {
+    switch (action?.name) {
+      case 'delete':
+        this.deleteCallback(action);
+        break;
+
+      default:
+        break;
+    }
     console.log('ListPostsComponent -> tableActions -> action', action);
+  }
+
+  deleteCallback(action) {
+    const postID = action?.row?.id;
+    const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+      width: '350px',
+      data: {
+        message: `Do you want to delete the post id: ${postID}`,
+      },
+    });
+
+    dialogRef
+      .afterClosed()
+      .pipe(
+        filter((result) => !!result),
+        switchMap(() => this.postService.deletePost(postID)),
+        tap((it) => console.log('Tap -> :', it))
+        // tap(() => this.postService.getPosts())
+      )
+      .subscribe();
   }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,14 +2,20 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { MaterialModule } from './../material/material.module';
+import { ConfirmationDialogComponent } from './components/confirmation-dialog/confirmation-dialog.component';
 import { TableActionsComponent } from './components/table-actions/table-actions.component';
 import { TableComponent } from './components/table/table.component';
 
-const sharedComponents = [TableComponent, TableActionsComponent];
+const sharedComponents = [
+  TableComponent,
+  TableActionsComponent,
+  ConfirmationDialogComponent,
+];
 
 @NgModule({
   declarations: [sharedComponents],
   exports: [sharedComponents],
+  entryComponents: [sharedComponents],
   imports: [CommonModule, MaterialModule],
 })
 export class SharedModule {}
