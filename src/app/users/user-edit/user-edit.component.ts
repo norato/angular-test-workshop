0f070af010b23a8268e578008fad1949ec29c79f
Felipe Norato <fnoratol@gmail.com>
2022-02-10 08:52:53 -0300

    app working

--- src/app/users/user-edit/user-edit.component.ts
+++ src/app/users/user-edit/user-edit.component.ts
@@ -33,7 +33,7 @@ export class UserEditComponent implements OnInit, OnDestroy {
   ) {}
 
   ngOnDestroy() {
-    this.destroy$.next();
+    this.destroy$.next(true);
     this.destroy$.complete();
   }
 
